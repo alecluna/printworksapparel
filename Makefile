296f552ec9ae0eea04f32b288673b3e77cbe80cf
Christine Tu <christine@Christines-MacBook-Air.local>
2019-03-06 18:32:12 -0800

    re-upload

--- Makefile
+++
@@ -1,6 +0,0 @@
-SECRET_KEY := 'stingersup'
-export SECRET_KEY
-
-runserver:
-	pg_ctl -D /usr/local/var/postgres restart
-	python3 manage.py runserver
--- saleor/static/dashboard-next/siteSettings/urls.ts
+++
@@ -1,9 +0,0 @@
-import * as urlJoin from "url-join";
-
-const siteSettingsSection = "/site-settings";
-
-export const siteSettingsPath = siteSettingsSection;
-export const siteSettingsUrl = siteSettingsPath;
-
-export const siteSettingsAddKeyPath = urlJoin(siteSettingsSection, "addKey");
-export const siteSettingsAddKeyUrl = siteSettingsAddKeyPath;
