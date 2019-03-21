da3665f40afc7a00f05dd41b3d26fb61754f2ceb
Alec Luna <alecluna@csus.edu>
2019-03-21 13:18:58 -0700

    changes to async function

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,42 @@
 //async fetch() to the lambda function
 const fetchUsers = async () =>
-  await fetch("/.netlify/functions/getYelpReviews");
+  await (await fetch("/.netlify/functions/getYelpReviews")).json();
 //await fetch("http://localhost:9000/getYelpReviews");
 
-fetchUsers()
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
+fetchUsers().then(data => {
+  console.log(data);
 
-    reviewList = document.querySelector("#reviews");
+  reviewList = document.querySelector("#reviews");
 
-    //generate HTML from api
-    data.reviews.forEach(review => {
-      const listElem = document.createElement("li");
-      listElem.className = "list-group-item";
+  //generate HTML from api
+  data.reviews.forEach(review => {
+    const listElem = document.createElement("li");
+    listElem.className = "list-group-item";
 
-      const reviewHeading = document.createElement("h4");
-      reviewHeading.className = "mb-1";
-      reviewHeading.appendChild(document.createTextNode(review.user.name));
-      listElem.appendChild(reviewHeading);
+    const reviewHeading = document.createElement("h4");
+    reviewHeading.className = "mb-1";
+    reviewHeading.appendChild(document.createTextNode(review.user.name));
+    listElem.appendChild(reviewHeading);
 
-      const ratingStars = document.createElement("h4");
-      ratingStars.className = "mb-1";
-      ratingStars.appendChild(document.createTextNode(review.rating));
-      listElem.appendChild(ratingStars);
+    const ratingStars = document.createElement("h4");
+    ratingStars.className = "mb-1";
+    ratingStars.appendChild(document.createTextNode(review.rating));
+    listElem.appendChild(ratingStars);
 
-      /////// This is causing me some issues right now
-      /////// My guess is CORS is preventing retreival of images from their API.
-      const userImg = document.createElement("img");
-      userImg.className = "rounded";
-      userImg.setAttribute("src", review.user.profile_url);
-      listElem.appendChild(userImg);
+    /////// This is causing me some issues right now
+    /////// My guess is CORS is preventing retreival of images from their API.
+    const userImg = document.createElement("img");
+    userImg.className = "rounded";
+    userImg.setAttribute("src", review.user.profile_url);
+    listElem.appendChild(userImg);
 
-      //also, we can fix this link later on
-      const link = document.createElement("a");
-      link.appendChild(document.createTextNode(review.text));
-      link.href = review.url;
-      link.target = "_blank";
-      listElem.appendChild(link);
+    //also, we can fix this link later on
+    const link = document.createElement("a");
+    link.appendChild(document.createTextNode(review.text));
+    link.href = review.url;
+    link.target = "_blank";
+    listElem.appendChild(link);
 
-      reviewList.appendChild(listElem);
-    });
+    reviewList.appendChild(listElem);
   });
+});
