3cb0cea398f6147669027a8ae64d58051cbf67b7
Alec Luna <alecluna@csus.edu>
2019-03-21 13:40:10 -0700

    removed hidden env vars for testing

--- src/functions/getYelpReviews.js
+++ src/functions/getYelpReviews.js
@@ -2,11 +2,6 @@ const axios = require("axios");
 
 exports.handler = function(event, context, callback) {
   const { API_URL, API_KEY } = process.env;
-
-  // const API_URL =
-  //   "https://api.yelp.com/v3/businesses/0ZWQpLWixYOquAAiRBmzSw/reviews";
-  // const API_KEY =
-  //   "QBeTWr5Doi4st1AYTKXNzleVU-4lK5b3AenvaP7AIMJmUt9ezpZ_iZ6bgkD5iNhypGw7l9dZ0nwp5sa5l-DeFwCj3wfh2TJmtZ75WiXucMcJURR9urfvHbqlF_iHXHYx";
   const AUTH_TOKEN = "Bearer " + API_KEY;
 
   const send = body => {
