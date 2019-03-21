f802aa14abc2dab69ff2dfa23e7984c358776ca0
Alec Luna <alecluna@csus.edu>
2019-03-21 13:03:57 -0700

    replace function with lambda path

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 //async fetch() to the lambda function
-const fetchUsers = async () =>
-  await fetch("/.netlify/functions/getYelpReviews");
+const fetchUsers = async () => await fetch("/.netlify/lambda/getYelpReviews");
 //await fetch("http://localhost:9000/getYelpReviews");
 
 fetchUsers()
