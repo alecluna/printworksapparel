cfc287efee95c0025dd6b3a22eb10a87b01ee16d
Alec Luna <alecluna@csus.edu>
2019-03-21 13:02:10 -0700

    fix broken lambda and test branch preview

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 //async fetch() to the lambda function
 const fetchUsers = async () =>
-  //await fetch("http://localhost:9000/getYelpReviews");
   await fetch("/.netlify/functions/getYelpReviews");
+//await fetch("http://localhost:9000/getYelpReviews");
 
 fetchUsers()
   .then(response => response.json())
