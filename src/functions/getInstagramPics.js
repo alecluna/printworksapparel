ec395235edfed0b1512d0c222c352dcbd297441b
Danny Gastelum <gastelumdf@gmail.com>
2019-04-09 15:38:03 -0700

    testing lambda function for instagram

--- src/functions/getInstagramPics.js
+++ src/functions/getInstagramPics.js
@@ -2,30 +2,29 @@
 const axios = require('axios')
 
 exports.handler = function (event, context, callback) {
-    // process.env is a way for us to export our sensitive data that 
+    // process.env is a way for us to export our sensitive data that
     // we dont want people to see to the netlify server
     //const { API_URL, API_KEY } = process.env
-    const API_URL;
-    const API_KEY;
-    
-    const AUTH_TOKEN = 'Bearer ' /* + put your API_URL here, this would be the endpoint */
-    
-    // if the next event is a 'GET', run this function() 
+    const ACCESS_TOKEN = `12144870609.53baa6c.8b9bd2a417414fffb820c1e551e137ad`;
+    const API_URL = `https://api.instagram.com/v1/users/self/media/recent?access_token=${ACCESS_TOKEN}`;
+    //const API_KEY;
+
+    //const AUTH_TOKEN = 'Bearer ' /* + put your API_URL here, this would be the endpoint */
+
+    // if the next event is a 'GET', run this function()
     if (event.httpMethod == 'GET') {
         getInstagramImages()
     }
 
-    // this is an arrow function in javascript, this is a fancy way of defining 
-    // a function, basically syntactic sugar. But here we make the actual request to the 
-    // instagram api 
+    // this is an arrow function in javascript, this is a fancy way of defining
+    // a function, basically syntactic sugar. But here we make the actual request to the
+    // instagram api
     const getInstagramImages = () => {
         axios
-            .get(API_URL, {
-                //header generated for the request to instagram
-                headers: {
-                    Authorization: AUTH_TOKEN,
-                    type: 'GET',
-                },
+            .get(API_URL,{
+              headers:{
+                type:'GET'
+              }
             })
             .then(response => send(response.data).catch(error => send(error)))
     }
@@ -40,7 +39,7 @@ exports.handler = function (event, context, callback) {
                 //This is to prevent CORS which is a stupid error in web dev
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Headers':
-                    'Origin, X-Requested-With, Content-Type, Accept',
+                'Origin, X-Requested-With, Content-Type, Accept',
             },
         })
     }
