d148c790ada5711a8a75f1069205583e2e905e46
Alec Luna <alecluna@csus.edu>
2019-04-12 11:26:44 -0700

    small changes

--- src/javascript/requestImages.js
+++ src/javascript/requestImages.js
@@ -1,25 +1,24 @@
 const fetchInstagram = async () =>
-    await (await fetch('/.netlify/functions/getYelpReviews')).json()
+    await (await fetch('/.netlify/functions/getInstagramPics')).json()
 
 fetchInstagram().then(response => {
     console.log(response)
 
-    images = document.querySelector('#igimages');
- 
-    response.forEach(function(data){
-        
-        const listElem = document.createElement("li");
-        listElem.style.listStyle = "none";
-        listElem.style.display = "inline";
-        listElem.style.padding = "5px";
-        listElem.className = "gallery-header";
-        
-        const image = document.createElement("img");
-        image.className = "gallery-image-tile";
-        image.setAttribute("src", data.images.low_resolution.url);
-        image.setAttribute("href", data.images.low_resolution.url)
-        listElem.appendChild(image);
+    images = document.querySelector('#igimages')
 
-        images.appendChild(listElem);
+    response.forEach(function(data) {
+        const listElem = document.createElement('li')
+        listElem.style.listStyle = 'none'
+        listElem.style.display = 'inline'
+        listElem.style.padding = '5px'
+        listElem.className = 'gallery-header'
+
+        const image = document.createElement('img')
+        image.className = 'gallery-image-tile'
+        image.setAttribute('src', data.images.low_resolution.url)
+        image.setAttribute('href', data.images.low_resolution.url)
+        listElem.appendChild(image)
+
+        images.appendChild(listElem)
     })
-});
+})
