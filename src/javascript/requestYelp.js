b6f0c07554528b6cdcabd5a50bc7a2b1539f32e6
Alec Luna <alecluna@csus.edu>
2019-04-28 14:53:23 -0700

    added error icon if Yelp api doesnt load on page

--- src/javascript/requestYelp.js
+++ src/javascript/requestYelp.js
@@ -2,31 +2,38 @@
 const fetchUsers = async () =>
     await (await fetch('/.netlify/functions/getYelpReviews')).json()
 
-fetchUsers().then(data => {
-    reviewList = document.querySelector('#reviews')
+fetchUsers()
+    .then(data => {
+        reviewList = document.querySelector('#reviews')
 
-    //generate HTML from api
-    data.reviews.forEach(review => {
-        const listElem = document.createElement('li')
-        listElem.className = 'review-list'
+        //generate HTML from api
+        data.reviews.forEach(review => {
+            const listElem = document.createElement('li')
+            listElem.className = 'review-list'
 
-        const reviewHeading = document.createElement('h3')
-        reviewHeading.className = 'review-heading'
-        reviewHeading.appendChild(document.createTextNode(review.user.name))
-        listElem.appendChild(reviewHeading)
+            const reviewHeading = document.createElement('h3')
+            reviewHeading.className = 'review-heading'
+            reviewHeading.appendChild(document.createTextNode(review.user.name))
+            listElem.appendChild(reviewHeading)
 
-        const ratingStars = document.createElement('img')
-        ratingStars.className = 'yelp-stars'
-        ratingStars.src = './images/yelp-stars.png'
-        listElem.appendChild(ratingStars)
+            const ratingStars = document.createElement('img')
+            ratingStars.className = 'yelp-stars'
+            ratingStars.src = './images/yelp-stars.png'
+            listElem.appendChild(ratingStars)
 
-        const link = document.createElement('a')
-        link.appendChild(document.createTextNode(review.text))
-        link.className = 'review-text'
-        link.href = review.url
-        link.target = '_blank'
-        listElem.appendChild(link)
+            const link = document.createElement('a')
+            link.appendChild(document.createTextNode(review.text))
+            link.className = 'review-text'
+            link.href = review.url
+            link.target = '_blank'
+            listElem.appendChild(link)
 
-        reviewList.appendChild(listElem)
+            reviewList.appendChild(listElem)
+        })
     })
-})
+    .catch(() => {
+        reviewList = document.querySelector('#reviews')
+        const errorIcon = document.createElement('i')
+        errorIcon.className = 'fas fa-exclamation-circle fa-4x yelp-error'
+        reviewList.appendChild(errorIcon)
+    })
