9bd0ebcf48556c019a3c60b185704c32aec5822d
Alec Luna <alecluna@csus.edu>
2019-05-09 01:21:23 -0700

    removed extra gallery test code

--- src/testing/testEstimate.js
+++ src/testing/testEstimate.js
@@ -29,23 +29,3 @@ const puppeteer = require('puppeteer');
 
     await browser.close()
 })()
-
-
-;(async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page
-        .goto('http://127.0.0.1:5500/src/gallery.html', {
-            waitUntil: 'load',
-            timeout: 0,
-        })
-        .then(() => {
-            console.log('success')
-        })
-        .catch(res => {
-            console.log('fails', res)
-        })
-    await page.screenshot({ path: 'Gallery.png' })
-
-    await browser.close()
-})()
